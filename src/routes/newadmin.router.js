e757f83606c7ee65563fc6dc129717ac86c33485
Vivekkumarv123 <vivekkumarv273@gmail.com>
2025-05-08 03:13:14 -0700

    create get test and route

--- src/routes/newadmin.router.js
+++ src/routes/newadmin.router.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createAdmin, createAdmintest, getAllTestDetails, saveGenerateTestResult, getTestDetailsById, getTestQuestionsWithAnswers, getTestSummariesForAllStudents, loginAdmin, updateTest, dashboardStudentData, getTestResults, getProfile } from '../controller/newadmin.controller.js';
+import { createAdmin, createAdmintest,getTestData, updateProfile, getAllTestDetails, saveGenerateTestResult, getTestDetailsById, getTestQuestionsWithAnswers, getTestSummariesForAllStudents, loginAdmin, updateTest, dashboardStudentData, getTestResults, getProfile } from '../controller/newadmin.controller.js';
 import { verifyToken } from '../middleware/jwtDecoder.middleware.js';
 
 
@@ -9,6 +9,7 @@ router.post("/signup", createAdmin);
 router.post("/login", loginAdmin);
 router.get("/student", getTestSummariesForAllStudents);
 router.post("/admintest", createAdmintest);
+router.get("/getTestData",verifyToken, getTestData);
 router.get("/test-data", getAllTestDetails);
 router.post("/test-data-by-id", getTestDetailsById);
 router.post("/get-questions", getTestQuestionsWithAnswers);
@@ -17,5 +18,6 @@ router.post("/update-test", updateTest);
 router.post("/student-data", dashboardStudentData);
 router.post("/test-result", getTestResults);
 router.get("/getProfile",verifyToken, getProfile);
+router.put("/updateProfile",verifyToken, updateProfile);
 
 export default router;

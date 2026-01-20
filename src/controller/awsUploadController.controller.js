8df4e299684ba73f2ddfd34bdbad600974ec883d
VinayakFrontend <guptavinayak1614@gmail.com>
2026-01-20 15:13:56 +0530

    updated s3

--- src/controller/awsUploadController.controller.js
+++ src/controller/awsUploadController.controller.js
@@ -1,9 +1,9 @@
-import AWS from "aws-sdk";
+import {S3Client} from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Configure AWS
-const s3 = new AWS.S3({
+const s3 = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
